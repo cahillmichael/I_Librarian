4895c302799e09ee5e1f6cbc34cca75e3647c851
michaelc <michaelc@carimus.com>
2022-03-20 17:46:17 -0400

    client side queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import {gql} from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: bookData) {
+        saveBook(input: $input) {
+            username
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                bookId
+            }
+        }
+    }
+`;
